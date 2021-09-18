a1a44012e073aab9210c53fe70552ea9eb624522
PraydE007 <prayde.niste@gmail.com>
2021-09-19 00:05:40 +0300

    upload

---
+++ Assets/CubeController.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+	public Color startColor = Color.black;
+	public float downStep = 5f;
+	public float delay = 1f;
+	public float stackDelay = 1f;
+
+	private MeshRenderer renderer = null;
+
+	private Color targetColor;
+	private Vector3 startPoint;
+	private Vector3 middlePoint;
+	private Vector3 endPoint;
+
+	private Vector3 realStartPoint;
+
+	private Vector3 offset;
+
+	private void Start()
+	{
+		startColor = new Color(startColor.r, startColor.g, startColor.b, 0f);
+		realStartPoint = transform.position;
+
+		renderer = GetComponent<MeshRenderer>();
+		renderer.material.color = startColor;
+
+		delay += stackDelay;
+
+		Recalculate();
+
+		targetColor = new Color(
+			renderer.material.color.r,
+			renderer.material.color.g,
+			renderer.material.color.b,
+			1f
+		);
+
+		StartCoroutine(CustomUpdate());
+	}
+
+	public static void RecalculateAll()
+	{
+		CubeController[] cubes = GameObject.FindObjectsOfType<CubeController> ();
+
+		foreach (CubeController cube in cubes)
+		{
+			cube.Recalculate();
+		}
+	}
+
+	private void Recalculate()
+	{
+		if (OffsetController.singleton == null)
+			return;
+
+		offset = OffsetController.singleton.CalculateOffset(
+			realStartPoint.x,
+			realStartPoint.y,
+			realStartPoint.z
+		);
+
+		// Debug.Log(name + " new offset: " + offset);
+
+		startPoint = realStartPoint + offset;
+		middlePoint = realStartPoint + (Vector3.down * downStep);
+		endPoint = realStartPoint + (Vector3.down * (downStep * 2) + offset);
+	}
+
+	private IEnumerator CustomUpdate()
+	{
+		while (true)
+		{
+			transform.position = startPoint;
+			renderer.material.color = startColor;
+
+			yield return new WaitForSeconds(delay);
+
+			while (Vector3.Distance(transform.position, middlePoint) > 0.1f)
+			{
+				transform.position = Vector3.Lerp(transform.position, middlePoint, 0.01f);
+				renderer.material.color = Color.Lerp(renderer.material.color, targetColor, 0.01f);
+
+				yield return null;
+			}
+			while (Vector3.Distance(transform.position, endPoint) > 0.1f)
+			{
+				transform.position = Vector3.Lerp(transform.position, endPoint, 0.01f);
+				renderer.material.color = Color.Lerp(renderer.material.color, startColor, 0.01f);
+
+				yield return null;
+			}
+		}
+	}
+}
---
+++ Assets/OffsetController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OffsetController : MonoBehaviour
+{
+	public bool sinX = false;
+	public bool sinY = false;
+	public bool sinZ = false;
+	public bool cosX = false;
+	public bool cosY = false;
+	public bool cosZ = false;
+	public bool tanX = false;
+	public bool tanY = false;
+	public bool tanZ = false;
+	public bool ctgX = false;
+	public bool ctgY = false;
+	public bool ctgZ = false;
+
+	public static OffsetController singleton = null;
+
+	private void Awake()
+	{
+		singleton = this;
+	}
+
+	public Vector3 CalculateOffset(float x, float y, float z)
+	{
+		Vector3 offset = Vector3.zero;
+
+		offset += sinX ? new Vector3(0, Mathf.Sin(x), 0) : Vector3.zero;
+		offset += sinY ? new Vector3(0, Mathf.Sin(y), 0) : Vector3.zero;
+		offset += sinZ ? new Vector3(0, Mathf.Sin(z), 0) : Vector3.zero;
+		offset += cosX ? new Vector3(0, Mathf.Cos(x), 0) : Vector3.zero;
+		offset += cosY ? new Vector3(0, Mathf.Cos(y), 0) : Vector3.zero;
+		offset += cosZ ? new Vector3(0, Mathf.Cos(z), 0) : Vector3.zero;
+		offset += tanX ? new Vector3(0, Mathf.Tan(x), 0) : Vector3.zero;
+		offset += tanY ? new Vector3(0, Mathf.Tan(y), 0) : Vector3.zero;
+		offset += tanZ ? new Vector3(0, Mathf.Tan(z), 0) : Vector3.zero;
+		offset += ctgX ? new Vector3(0, 1 / Mathf.Tan(x), 0) : Vector3.zero;
+		offset += ctgY ? new Vector3(0, 1 / Mathf.Tan(y), 0) : Vector3.zero;
+		offset += ctgZ ? new Vector3(0, 1 / Mathf.Tan(z), 0) : Vector3.zero;
+
+		return offset;
+	}
+
+	private void OnValidate()
+	{
+		Debug.Log("Recalculating offset!");
+		CubeController.RecalculateAll();
+	}
+}
